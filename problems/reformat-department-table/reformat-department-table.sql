61b7219db26af89041cd21c16aba6e9403e1ec1c
Bui Nam <buikhacnam11@gmail.com>
2022-06-21 11:11:20 +0700

    reformat department table

--- problems/reformat-department-table/reformat-department-table.sql
+++ problems/reformat-department-table/reformat-department-table.sql
@@ -1,3 +1,11 @@
 SELECT * FROM girrafe.`department-table`;
 
 
+SELECT id, 
+	SUM(CASE WHEN month = 'jan' THEN revenue ELSE null END) AS Jan_Revenue,
+	SUM(CASE WHEN month = 'feb' THEN revenue ELSE null END) AS Feb_Revenue,
+	SUM(CASE WHEN month = 'mar' THEN revenue ELSE null END) AS Mar_Revenue,
+	SUM(CASE WHEN month = 'apr' THEN revenue ELSE null END) AS Apr_Revenue
+FROM girrafe.`department-table`
+GROUP BY id
+ORDER BY id;
